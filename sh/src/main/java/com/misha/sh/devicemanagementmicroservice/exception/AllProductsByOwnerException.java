8ce2e4de2e96db9c63752a929209bc10cf456f14
Mishar025 <mishamay583@gmail.com>
2024-09-03 19:28:31 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/AllProductsByOwnerException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class AllProductsByOwnerException extends RuntimeException {
+    public AllProductsByOwnerException(String msg) {
+        super(msg);
+    }
+}
