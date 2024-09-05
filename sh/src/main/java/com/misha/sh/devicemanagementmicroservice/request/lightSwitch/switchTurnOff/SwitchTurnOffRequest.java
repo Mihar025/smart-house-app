e6557ff9e81f86e352ede546e3a1738a2d22bf97
Mishar025 <mishamay583@gmail.com>
2024-09-04 22:27:47 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/request/lightSwitch/switchTurnOff/SwitchTurnOffRequest.java
@@ -0,0 +1,15 @@
+package com.misha.sh.devicemanagementmicroservice.request.lightSwitch.switchTurnOff;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SwitchTurnOffRequest {
+
+    private boolean isOn;
+
+
+}
