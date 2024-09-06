d20d0fdd290b80a1eb1501368a5924fb002460e2
Mishar025 <mishamay583@gmail.com>
2024-09-05 20:56:03 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/BusinessFindDrinkException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class BusinessFindDrinkException extends RuntimeException {
+    public BusinessFindDrinkException(String msg) {
+        super(msg);
+    }
+}
