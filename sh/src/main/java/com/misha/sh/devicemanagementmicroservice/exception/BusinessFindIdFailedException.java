456e7cb139ad0ad4ad419cbf5be7f65f91ea751c
Mishar025 <mishamay583@gmail.com>
2024-09-05 20:56:28 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/BusinessFindIdFailedException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class BusinessFindIdFailedException extends RuntimeException {
+    public BusinessFindIdFailedException(String msg) {
+        super(msg);
+    }
+}
