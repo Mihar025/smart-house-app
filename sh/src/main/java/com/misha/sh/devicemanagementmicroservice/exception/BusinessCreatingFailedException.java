a95055317be8b4246768d283f50158266f55ae5c
Mishar025 <mishamay583@gmail.com>
2024-09-05 20:55:10 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/BusinessCreatingFailedException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class BusinessCreatingFailedException extends RuntimeException {
+    public BusinessCreatingFailedException(String msg) {
+        super(msg);
+    }
+}
