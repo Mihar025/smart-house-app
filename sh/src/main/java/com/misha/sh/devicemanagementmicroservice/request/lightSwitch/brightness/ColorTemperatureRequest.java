30b7a507f31e7e30932c62ea616beac4d3ba2d7e
Mishar025 <mishamay583@gmail.com>
2024-09-06 21:05:12 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/request/lightSwitch/brightness/ColorTemperatureRequest.java
@@ -0,0 +1,16 @@
+package com.misha.sh.devicemanagementmicroservice.request.lightSwitch.brightness;
+
+import com.misha.sh.devicemanagementmicroservice.model.swtichLight.ColorTemperature;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ColorTemperatureRequest {
+
+    private ColorTemperature colorTemperature;
+    private Integer ownerId;
+
+}
