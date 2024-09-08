f958b6123a977fc3b764d7503d63bd0111edf143
Mishar025 <mishamay583@gmail.com>
2024-09-08 00:01:43 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/model/device/DeviceType.java
@@ -0,0 +1,15 @@
+package com.misha.sh.devicemanagementmicroservice.model.device;
+
+public enum DeviceType {
+    THERMOSTAT,
+    LOCK,
+    CAMERA,
+    SPEAKER,
+    CHARGER,
+    REMOTE_CONTROLLER,
+    MULTI_MEDIA,
+    SENSOR,
+    SWITCH,
+    OUTLET,
+    LIGHT
+}
