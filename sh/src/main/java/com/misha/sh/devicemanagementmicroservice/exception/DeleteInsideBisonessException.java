70bd7cea464f2fcae7dd5428c27a2736e5446d0c
Mishar025 <mishamay583@gmail.com>
2024-09-07 23:58:37 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/DeleteInsideBisonessException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class DeleteInsideBisonessException extends RuntimeException {
+    public DeleteInsideBisonessException(String msg) {
+        super(msg);
+    }
+}
