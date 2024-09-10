69b455b3448c08a4de53cbd7aa40f81595b51eef
Mishar025 <mishamay583@gmail.com>
2024-09-09 22:46:27 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/mapper/DoorLockMapper.java
@@ -0,0 +1,33 @@
+package com.misha.sh.devicemanagementmicroservice.mapper;
+
+import com.misha.sh.devicemanagementmicroservice.model.doorLock.DoorLock;
+import com.misha.sh.devicemanagementmicroservice.request.doorLock.DoorLockResponse;
+import com.misha.sh.devicemanagementmicroservice.request.doorLock.DoorLockStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DoorLockMapper {
+
+    public DoorLockResponse toDoorLockResponse(DoorLock doorLock) {
+        return DoorLockResponse.builder()
+                .deviceId(doorLock.getId())
+                .deviceName(doorLock.getDeviceName())
+                .deviceType(doorLock.getDeviceType())
+                .manufacturer(doorLock.getManufacturer())
+                .deviceModel(doorLock.getDeviceModel())
+                .serialNumber(doorLock.getSerialNumber())
+                .status(doorLock.getStatus())
+                .lockStatus(doorLock.getLockStatus())
+                .lockMechanism(doorLock.getLockMechanism())
+                .ownerId(doorLock.getUser().getId())
+                .build();
+    }
+
+    public DoorLockStatus toDoorLockStatus(DoorLock doorLock){
+        return DoorLockStatus.builder()
+                .doorLockId(doorLock.getId())
+                .isOpened(doorLock.isOpened())
+                .lastOpenedTime(doorLock.getLastOpenedAt())
+                .build();
+    }
+}
