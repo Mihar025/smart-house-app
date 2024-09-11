2d960c4f047c91862eca52d7f52ba491af9b3eab
Mishar025 <mishamay583@gmail.com>
2024-09-10 22:37:45 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/Controllers/DeviceController.java
@@ -0,0 +1,15 @@
+package com.misha.sh.devicemanagementmicroservice.Controllers;
+
+import com.misha.sh.devicemanagementmicroservice.service.DevicService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/device")
+public class DeviceController {
+
+    private final DevicService devicService;
+
+}
