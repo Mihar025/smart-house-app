a9396ed952dca0ef4075736b4245d4efde77ef8d
Mishar025 <mishamay583@gmail.com>
2024-09-13 22:23:12 -0400

    FirstCommit

--- sh/src/main/java/com/misha/sh/devicemanagementmicroservice/Controllers/DoorLockController.java
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/Controllers/DoorLockController.java
@@ -1,10 +1,12 @@
 package com.misha.sh.devicemanagementmicroservice.Controllers;
 
-import com.misha.sh.devicemanagementmicroservice.request.doorLock.DoorLockAccessCodeRequest;
-import com.misha.sh.devicemanagementmicroservice.request.doorLock.DoorLockRequest;
-import com.misha.sh.devicemanagementmicroservice.request.doorLock.DoorLockResponse;
-import com.misha.sh.devicemanagementmicroservice.request.doorLock.DoorLockStatus;
+import com.misha.sh.devicemanagementmicroservice.exception.BusinessException;
+import com.misha.sh.devicemanagementmicroservice.model.User;
+import com.misha.sh.devicemanagementmicroservice.pagination.PageResponse;
+import com.misha.sh.devicemanagementmicroservice.request.doorLock.*;
+import com.misha.sh.devicemanagementmicroservice.request.smartOutlet.SmartOutletResponse;
 import com.misha.sh.devicemanagementmicroservice.service.doorLockService.DoorLockService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,27 +21,55 @@ public class DoorLockController {
     private final DoorLockService doorLockService;
 
 
+
+    @GetMapping("/{lockId}")
+    public ResponseEntity<DoorLockResponse> getLockById(@PathVariable Integer lockId, Authentication authentication) {
+        try {
+            DoorLockResponse response = doorLockService.findLockById(lockId, authentication);
+            return ResponseEntity.ok(response);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (BusinessException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<DoorLockResponse>> findAllDoorLock(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            Authentication authentication) {
+        PageResponse<DoorLockResponse> response = doorLockService.findAllDoorLocks(authentication, size, page);
+        return ResponseEntity.ok(response);
+    }
+
+
+
     @PostMapping
     public ResponseEntity<DoorLockResponse> addDoorLock(@RequestBody DoorLockRequest doorLockRequest, Authentication authentication) {
         DoorLockResponse response = doorLockService.addDoorLock(doorLockRequest, authentication);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+
     @PutMapping("/{doorLockId}/access-code")
-    public ResponseEntity<Void> changeAccessCode(Authentication authentication, @RequestBody Integer accessCode, @PathVariable Integer doorLockId) {
-        doorLockService.changeAccessCode(authentication, accessCode, doorLockId);
+    public ResponseEntity<Void> changeAccessCode(Authentication authentication, @RequestBody ChangeDoorLockRequest doorLockRequest, @PathVariable Integer doorLockId) {
+        Integer pinCode = Integer.parseInt(doorLockRequest.getDoorCode());
+        doorLockService.changeAccessCode(authentication, pinCode , doorLockId);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/{doorLockId}/open")
     public ResponseEntity<DoorLockStatus> openDoorLock(Authentication authentication, @PathVariable Integer doorLockId, @RequestBody DoorLockAccessCodeRequest accessCodeRequest) {
-        DoorLockStatus status = doorLockService.openDoorLock(authentication, doorLockId, accessCodeRequest);
+        Integer accessCode = Integer.parseInt(accessCodeRequest.getAccessCode());
+        DoorLockStatus status = doorLockService.openDoorLock(authentication, doorLockId, accessCode);
         return ResponseEntity.ok(status);
     }
 
     @PostMapping("/{doorLockId}/close")
     public ResponseEntity<DoorLockStatus> closeDoorLock(Authentication authentication, @PathVariable Integer doorLockId, @RequestBody DoorLockAccessCodeRequest accessCodeRequest) {
-        DoorLockStatus status = doorLockService.closeDoorLock(authentication, doorLockId, accessCodeRequest);
+        Integer accessCode = Integer.parseInt(accessCodeRequest.getAccessCode());
+        DoorLockStatus status = doorLockService.closeDoorLock(authentication, doorLockId, accessCode);
         return ResponseEntity.ok(status);
     }
 
