2c364ae335016e80a99d72c0220ddcbbfff6c654
Mishar025 <mishamay583@gmail.com>
2024-09-18 21:38:31 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/DishesPictureException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class DishesPictureException extends RuntimeException {
+    public DishesPictureException(String msg) {
+        super(msg);
+    }
+}
