7ced1656cb5d6d4643eecbbd7ebe3e7cc540bb26
Mishar025 <mishamay583@gmail.com>
2024-09-18 21:38:01 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/DishesFindFailedException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class DishesFindFailedException extends RuntimeException {
+    public DishesFindFailedException(String msg) {
+    super(msg);
+    }
+}
