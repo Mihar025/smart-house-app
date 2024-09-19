5ea2432b5198c04246218dd532a46f0306390bae
Mishar025 <mishamay583@gmail.com>
2024-09-18 21:39:47 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/DrinkFindByOwnerException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class DrinkFindByOwnerException extends RuntimeException {
+    public DrinkFindByOwnerException(String msg) {
+        super(msg);
+    }
+}
