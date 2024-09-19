52212973f9d456bafc3fc100161247bbaefd6b4c
Mishar025 <mishamay583@gmail.com>
2024-09-18 21:41:13 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/EmailNotFOundException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class EmailNotFOundException extends RuntimeException {
+    public EmailNotFOundException(String msg) {
+        super(msg);
+    }
+}
