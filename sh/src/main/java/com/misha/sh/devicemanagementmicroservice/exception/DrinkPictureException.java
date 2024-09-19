2d602a3f2450a0cde9543473a33487b1d217c448
Mishar025 <mishamay583@gmail.com>
2024-09-18 21:40:20 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/DrinkPictureException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class DrinkPictureException extends RuntimeException {
+    public DrinkPictureException(String msg) {
+        super(msg);
+    }
+}
