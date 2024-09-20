8ebbc445e6b52439c8df8b761ddc7d0e08965dec
Mishar025 <mishamay583@gmail.com>
2024-09-19 21:47:06 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/FindAllBusinessesWithoutDeliveryException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class FindAllBusinessesWithoutDeliveryException extends RuntimeException {
+    public FindAllBusinessesWithoutDeliveryException(String msg) {
+        super(msg);
+    }
+}
