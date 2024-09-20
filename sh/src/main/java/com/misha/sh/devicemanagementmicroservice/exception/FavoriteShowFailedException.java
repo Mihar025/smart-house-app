873e5fd1c5c800456aeede242661cb432a1dae0d
Mishar025 <mishamay583@gmail.com>
2024-09-19 21:46:29 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/FavoriteShowFailedException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class FavoriteShowFailedException extends RuntimeException {
+    public FavoriteShowFailedException(String message) {
+        super(message);
+    }
+}
