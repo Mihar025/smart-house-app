d0c62b3bbb46150ff944d00e65dad2b7ec284937
Mishar025 <mishamay583@gmail.com>
2024-09-19 21:47:58 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/FoodAddingException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class FoodAddingException extends RuntimeException {
+    public FoodAddingException(String msg) {
+        super(msg);
+    }
+}
