007a036767475c64552e70c1ef4df164e12694e7
Mishar025 <mishamay583@gmail.com>
2024-09-19 21:46:48 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/FavoriteAddFailedException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class FavoriteAddFailedException extends RuntimeException {
+    public FavoriteAddFailedException(String msg) {
+        super(msg);
+    }
+}
