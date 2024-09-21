360dc4e45c3bbe8b42156422aa14683deadf3766
Mishar025 <mishamay583@gmail.com>
2024-09-20 20:18:10 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/InternalServerErrorException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class InternalServerErrorException extends Throwable {
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+}
