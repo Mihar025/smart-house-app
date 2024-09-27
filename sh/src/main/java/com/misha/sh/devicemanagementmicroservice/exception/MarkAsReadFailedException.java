fb5f91cc2c328419677e94ca98b743f9f6b14d31
Mishar025 <mishamay583@gmail.com>
2024-09-26 22:34:59 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/MarkAsReadFailedException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class MarkAsReadFailedException extends RuntimeException {
+    public MarkAsReadFailedException(String msg) {
+        super(msg);
+    }
+}
