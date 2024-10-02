a9bc9b54dcfa2a985aa43f6e5b7f60c09fc9ac39
Mishar025 <mishamay583@gmail.com>
2024-10-01 22:56:39 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/OrderDetailesFailed.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class OrderDetailesFailed extends RuntimeException {
+    public OrderDetailesFailed(String message) {
+        super(message);
+    }
+}
