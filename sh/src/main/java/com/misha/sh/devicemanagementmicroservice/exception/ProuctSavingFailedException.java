7e49a3a5a85ba346735528f26fe3866270ff135d
Mishar025 <mishamay583@gmail.com>
2024-10-01 22:57:33 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/ProuctSavingFailedException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class ProuctSavingFailedException extends RuntimeException {
+    public ProuctSavingFailedException(String msg) {
+        super(msg);
+    }
+}
