92b89475fb8d86d82c1a7644a8045bd151676fa5
Mishar025 <mishamay583@gmail.com>
2024-10-03 23:32:31 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/repository/TokenRepository.java
@@ -0,0 +1,14 @@
+package com.misha.sh.devicemanagementmicroservice.repository;
+
+
+
+import com.misha.sh.devicemanagementmicroservice.security.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+    Optional<Token> findByToken(String token);
+}
