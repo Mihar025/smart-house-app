de19b9a36f395dbaf536f78c6ef205a6ca9a423b
Mishar025 <mishamay583@gmail.com>
2024-10-03 23:33:14 -0400

    FirstCommit

---
+++ sh/src/main/java/com/misha/sh/devicemanagementmicroservice/exception/UnreadNotificationException.java
@@ -0,0 +1,7 @@
+package com.misha.sh.devicemanagementmicroservice.exception;
+
+public class UnreadNotificationException extends RuntimeException {
+    public UnreadNotificationException(String msg) {
+        super(msg);
+    }
+}
